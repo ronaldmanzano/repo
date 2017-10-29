05e1fef6cbf21074d2fce0412720a32243b79b0f
Ronald <nald_manzano@hotmail.com>
2017-10-29 15:39:41 +1100

    test

--- source/rms/Startup.cs
+++ source/rms/Startup.cs
@@ -14,6 +14,7 @@ namespace rms
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
